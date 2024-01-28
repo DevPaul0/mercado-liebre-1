2735380dc05b86951c640bf25bf5642aef472865
DevPaul0 <itspaulogimenez@gmail.com>
2024-01-28 19:55:39 -0300

    add forms register and login.

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express")
 const app = express()
 const port = 3030
 const path = require("path")
+
 app.use(express.static('public'))
+
 app.get("/",(req,res) => { 
-res.sendFile(path.join(__dirname,"views/home.html"))})
+    res.sendFile(path.join(__dirname,"views/home.html"))})
+
+app.get("/register",(req,res) => { 
+    res.sendFile(path.join(__dirname,"views/register.html"))})
+
+app.get("/login",(req,res) => { 
+    res.sendFile(path.join(__dirname,"views/login.html"))}) 
+    
 app.listen(port,() => console.log(`http://localhost:${port}`))  
